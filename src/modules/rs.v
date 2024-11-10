da29fc056c83f4b7c42dc952ed3e8a68dbe9ff90
algebraic-arima <lfk.alg@gmail.com>
2024-11-10 23:53:28 +0800

    some rs

--- src/modules/rs.v
+++ src/modules/rs.v
@@ -7,13 +7,53 @@ module rs (
 
     output wire rs_full,
 
+    input wire clear,
+
     // from decoder
     input wire dec_ready,
-    input wire [31:0] inst_addr
+    input wire [31:0] inst_addr,
+
+    // from rs
+    input wire rs_ready,
+    input wire [`ROB_WIDTH-1:0] rs_rob_id,
+    input wire [31 : 0] rs_value,
+
+    // from lsb
+    input wire lsb_ready,
+    input wire [`ROB_WIDTH-1:0] lsb_rob_id,
+    input wire [31 : 0] lsb_value,
+
+    // broadcast that a rob is ready
+    output reg ready,
+    output reg [`ROB_WIDTH-1:0] rob_id,
+    output reg [31:0] value,
 
     
 );
 
     reg busy [0:`RS_SIZE-1];
+    reg [3:0] inst_type[0:`RS_SIZE-1];
+    reg [8:0] inst_op[0:`RS_SIZE-1];
+    reg [31:0] vj[0:`RS_SIZE-1];
+    reg [31:0] vk[0:`RS_SIZE-1];
+    reg has_dj[0:`RS_SIZE-1];
+    reg has_dk[0:`RS_SIZE-1];
+    reg [`ROB_WIDTH-1:0] qj[0:`RS_SIZE-1]; // dependence of vj
+    reg [`ROB_WIDTH-1:0] qk[0:`RS_SIZE-1];
+    reg [31:0] a[0:`RS_SIZE-1];
+    reg [`ROB_WIDTH-1:0] rob_dest[0:`RS_SIZE-1]; // which rob depends on this
+    reg val_ready [0:`RS_SIZE-1];
+
+    wire [`RS_WIDTH-1:0] calc;
+    wire [`RS_WIDTH-1:0] idle;
+
+    genvar i;
+    generate 
+        for (i = 0; i < `RS_SIZE; i = i + 1) begin
+            assign calc[i] = busy[i] && (!has_dj[i] && !has_dk[i]);
+        end
+    endgenerate
+
+
 
 endmodule
